2c804d81c3df906d2f2987ba5fbdf7931feeec4d
Caique Porfirio <caiquepfr@hotmail.com>
2020-03-24 22:48:47 -0300

    Backend commit

---
+++ backend/src/database/migrations/20200324201936_create_incidents.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+  knex.schema.createTable("incidents", function(table) {
+    table.incremetns();
+    table.string("title").notNullable();
+    table.string("description").notNullable();
+    table.decimal("value").notNullable();
+
+    table.string("ong_id").notNullable();
+
+    table
+      .foreign("ong_id")
+      .references("id")
+      .inTable("ongs");
+  });
+};
+
+exports.down = function(knex) {
+  knex.schema.dropTable("incidents");
+};
